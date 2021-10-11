94fcd88ca0b34352a264a366e29a7601b3c7be77
Kcuevas95 <karlacuevasdesigns@gmail.com>
2021-10-11 01:05:02 +0000

    Added fetch

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -13,6 +13,82 @@ export function Home() {
 		"Walk the Dog"
 	]);
 
+	// Setting up useState
+	const [listArray, setListArray] = useState([]);
+
+	// URL to use
+	const apiURL = "https://assets.breatheco.de/apis/fake/todos/user/kcuevas95";
+
+	// UseEffect for retrieving stored values from database on mount
+	useEffect(() => {
+		// fetch(apiURL)
+		// 	.then(resp => {
+		// 		console.log(resp.status);
+		// 		if (resp.status >= 200 && resp.status < 300) {
+		// 			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		// 		} else {
+		// 			alert(
+		// 				`This is a client error, its your fault this is a ${resp.status}`
+		// 			);
+		// 			throw Error(`${resp.ok} ${resp.status}`);
+		// 		}
+		// 		// console.log(resp.ok); // will be true if the response is successfull
+		// 		// console.log(resp.status); // the status code = 200 or code = 400 etc.
+		// 	})
+		// 	.then(data => {
+		// 		//here is were your code should start after the fetch finishes
+		// 		// console.log(data); //this will print on the console the exact object received from the server
+		// 		setListArray(data);
+		// 	})
+		// 	.catch(error => {
+		// 		//error handling
+		// 		console.log("This is an error: ", error);
+		// 	});
+		initList();
+	}, []);
+
+	// UseEffect for updating Todo List array values
+	useEffect(() => {
+		// fetch(apiURL, {
+		// 	method: "PUT", // or 'POST'
+		// 	body: JSON.stringify(listArray), // data can be `string` or {object}!
+		// 	headers: {
+		// 		"Content-Type": "application/json"
+		// 	}
+		// })
+		// 	.then(res => res.json())
+		// 	.then(response => console.log("Success:", JSON.stringify(response)))
+		// 	.catch(error => console.error("Error:", error));
+		updateList();
+	}, [listArray]);
+
+	/* USING ASYNC */
+	const initList = async () => {
+		const response = await fetch(apiURL);
+		try {
+			const data = await response.json();
+			setListArray(data);
+		} catch (error) {
+			throw new Error(error);
+		}
+	};
+
+	const updateList = async () => {
+		const response = await fetch(apiURL, {
+			method: "PUT", // or 'POST'
+			body: JSON.stringify(listArray), // data can be `string` or {object}!
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+		try {
+			const data = await response.json();
+			console.log("Success:", JSON.stringify(data));
+		} catch (error) {
+			throw new Error(error);
+		}
+	};
+
 	let todo = variable.map((item, i) => {
 		return (
 			<li className="list-group-item item" key={i}>
